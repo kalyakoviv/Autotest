6e505c0fe27b204e7ee25ed3407f2b435a27a5ca
kalyakoviv <kalyakoviv@gmail.com>
2022-05-31 21:41:12 +0300

    AT

--- 1.py
+++ 1.py
@@ -1,6 +1,9 @@
-class my_class(object):
-    pass
+from selenium import webdriver
+driver = webdriver.Chrome()
 
+driver.get('https://www.youtube.com/')
+seearchbox = driver.find_element_by_xpath('//*[@id="search"]
+')
 
 
 
