26218f99fe4e7fe995cd278781c145c62f17aa94
kalyakoviv <kalyakoviv@gmail.com>
2022-06-01 20:33:00 +0300

    AU

--- 1.py
+++ 1.py
@@ -1,15 +1,10 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager 
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 driver.get('https://www.youtube.com/')
-
-# seearchbox = driver.find_element_by_xpath("//*[@id="search"]")
-# searchbox.send_keys('igor qa')
-
-# seearchbox = driver.find_element_by_xpath('//*[@id="search"]')
 
 time.sleep(5)
 
---
+++ test.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get("https://yandex.ru/")
