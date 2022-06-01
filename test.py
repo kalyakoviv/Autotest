8ac0369ca8c51da9adb0428e9dfafaba853f9020
kalyakoviv <kalyakoviv@gmail.com>
2022-06-02 00:06:14 +0300

    AU

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import time
 
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.maximize_window()
 
 driver.get("https://yandex.ru/")
 time.sleep(1)
@@ -14,11 +15,14 @@ search = driver.find_element_by_name("text")
 search.send_keys("Тензор")
 time.sleep(5)
 
+driver.find_element_by_xpath("/html/body/div[3]")
+time.sleep(5)
+
 search.send_keys(Keys.RETURN)
 time.sleep(5)
 
-tenzor = driver.find_elements_by_xpath('//*[@id="search-result"]/li[1]/div/div[1]/a/h2')
+tenzor = driver.find_element_by_xpath('//*[@id="search-result"]/li[1]/div/div[1]/div[1]/a/h2/span')
 tenzor.click()
-time.sleep(5)
+time.sleep(10)
 
 pass
