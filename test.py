6c47134801c149014f2b631744065b7d7dc65f71
kalyakoviv <kalyakoviv@gmail.com>
2022-06-01 22:58:18 +0300

    AU

--- test.py
+++ test.py
@@ -1,7 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+import time
+
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 driver.get("https://yandex.ru/")
+time.sleep(1)
+
+search = driver.find_element_by_name("text")
+
+search.send_keys("Тензор")
+time.sleep(5)
+
+search.send_keys(Keys.RETURN)
+time.sleep(5)
+
+tenzor = driver.find_elements_by_xpath('//*[@id="search-result"]/li[1]/div/div[1]/a/h2')
+tenzor.click()
+time.sleep(5)
+
+pass
