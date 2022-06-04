b46753b10e8402ae5d54649d60c07582bb43667a
kalyakoviv <kalyakoviv@gmail.com>
2022-06-04 23:41:04 +0300

    AI

--- УК_test/conftest.py
+++ УК_test/conftest.py
@@ -7,6 +7,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome(ChromeDriverManager().install())
+    browser.implicitly_wait(5)
     yield browser
     print("\nquit browser..")
     browser.quit()
