d41dcabb2c46a8f9df196da77f11129f8bc8d700
kalyakoviv <kalyakoviv@gmail.com>
2022-06-05 13:34:55 +0300

    AI

--- УК_test/Pages/main_page.py
+++ УК_test/Pages/main_page.py
@@ -11,4 +11,9 @@ class MainPage(BasePage):
     #     assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
 
     def should_be_search(self):
-        assert self.search_present(By.NAME, "text"), "Login link is not presented"
+        assert self.search_present(By.NAME, "text"), "The search field is missing"
+
+    def search_tenzor(self):
+        search_tenz = self.browser.find_element(By.NAME, "text")
+        search_tenz.click()
+        search_tenz.send_keys("Тензор")
--- УК_test/test_1.py
+++ УК_test/test_1.py
@@ -1,23 +1,31 @@
 from pages.main_page import MainPage
 import time
 
-
+# Зайти на yandex.ru
 def test_open(browser):
     link = "https://yandex.ru/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()
     time.sleep(1)
-#
-# def test_guest_should_see_login_link(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.should_be_login_link()
 
+# Проверить наличие поля поиска
 def test_should_be_search(browser):
     link = "https://yandex.ru/"
     page = MainPage(browser, link)
     page.open()
-    time.sleep(3)
+    time.sleep(1)
     page.should_be_search()
     time.sleep(7)
+
+# Ввести в поиск Тензор
+
+def test_tenzor(browser):
+    link = "https://yandex.ru/"
+    page = MainPage(browser, link)
+    page.open()
+    time.sleep(1)
+    page.should_be_search()
+    time.sleep(7)
+    page.search_tenzor()
+    time.sleep(2)
+
